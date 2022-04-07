1150c09280abedf629251433cdf7f6b4f079275d
Andres <andres.amenjivar@gmail.com>
2022-04-07 14:37:31 -0400

    Colour changed for Login, Search and Library. Added new files for Album favs (Library).

---
+++ app/src/main/java/com/cst2335/finalproject/FavDB.java
@@ -0,0 +1,85 @@
+package com.cst2335.finalproject;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class FavDB  extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "AlbumsDB";
+    public static final int DB_VERSION = 1;
+    public static final String TABLE_NAME = "favoriteTable";
+    public static final String KEY_ID = "id";
+    public static final String ALBUM_TITLE = "albumTitle";
+    public static final String ALBUM_IMAGE = "albumImage";
+    public static final String FAVORITE_STATUS = "fStatus";
+
+    private static String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+            + KEY_ID + " TEXT," + ALBUM_TITLE + " TEXT,"
+            + ALBUM_IMAGE + " TEXT," + FAVORITE_STATUS + " TEXT)";
+
+    public FavDB(Context context){ super(context,DATABASE_NAME,null,DB_VERSION);}
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    //to create an empty table
+    public void insertEmpty(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        // enter your value
+        for (int x = 1; x < 11; x++) {
+            cv.put(KEY_ID, x);
+            cv.put(FAVORITE_STATUS, "0");
+
+            db.insert(TABLE_NAME,null, cv);
+        }
+    }
+
+    // insert data into database
+    public void insertIntoTheDatabase(String album_title, int album_image, String id, String fav_status) {
+        SQLiteDatabase db;
+        db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(ALBUM_TITLE, album_title);
+        cv.put(ALBUM_IMAGE, album_image);
+        cv.put(KEY_ID, id);
+        cv.put(FAVORITE_STATUS, fav_status);
+        db.insert(TABLE_NAME,null, cv);
+        Log.d("FavDB Status", album_title + ", favstatus - "+fav_status+" - . " + cv);
+    }
+
+    // read all data
+    public Cursor read_all_data(String id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String sql = "select * from " + TABLE_NAME + " where " + KEY_ID+"="+id+"";
+        return db.rawQuery(sql,null,null);
+    }
+
+    // remove line from database
+    public void remove_fav(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String sql = "UPDATE " + TABLE_NAME + " SET  "+ FAVORITE_STATUS+" ='0' WHERE "+KEY_ID+"="+id+"";
+        db.execSQL(sql);
+        Log.d("remove", id.toString());
+
+    }
+
+    // select all favourite list
+    public Cursor select_all_favorite_list() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String sql = "SELECT * FROM "+TABLE_NAME+" WHERE "+FAVORITE_STATUS+" ='1'";
+        return db.rawQuery(sql,null,null);
+    }
+}
