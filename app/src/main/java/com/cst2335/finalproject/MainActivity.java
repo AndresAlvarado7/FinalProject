1214360fbd79e2c1c34a4f63830f7a68ea180f6c
TimMurillo <muri0025@algonquinlive.com>
2022-04-12 12:19:57 -0400

    tried to add progress bar through method and through a button but didnt work so just placed in comments

--- app/src/main/java/com/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/com/cst2335/finalproject/MainActivity.java
@@ -41,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
 //    private Button btn;
 //    private ProgressDialog loading;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
